3bf95198f4d88ec272d5509b6e86e1ef0d3c93ba
mangonob <534393806@qq.com>
2017-10-31 18:04:07 +0800

    Generic view

--- polls/views.py
+++ polls/views.py
@@ -1,40 +1,34 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.views import generic
 
 # Create your views here.
 
 from .models import *
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def dynamic_url(reqeust, url_tail):
-    return HttpResponse("Dynamic %s" % url_tail)
-
-
-def detail(request, question_id):
-    return render(request, 'polls/detail.html', {'question': get_object_or_404(Question, pk=question_id)})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'polls/question_detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
@@ -45,3 +39,8 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class ResultView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
